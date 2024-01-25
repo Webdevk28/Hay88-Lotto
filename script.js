6f94453ba534f27b48672598a8ea0c70a4c8cf77
Webdevk28 <117263501+Webdevk28@users.noreply.github.com>
2024-01-25 15:57:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", function () {
-hamburger.classList.toggle("active");
-navMenu.classList.toggle("active");
-});
-
-// =============================================================
-
-// date month year
-
-// const date = new Date();
-
-// let day = date.getDate();
-// let month = date.getMonth() +1;
-// let year = date.getFullYear();
-
-// document.querySelector(".day").innerHTML =  day + "/" + month + "/" + year;
-// document.querySelector(".day-1").innerHTML =  day-1 + "/" + month + "/" + year;
-// document.querySelector(".day-2").innerHTML =  day-2 + "/" + month + "/" + year;
-// document.querySelector(".day-3").innerHTML =  day-3 + "/" + month + "/" + year;
-// document.querySelector(".day-4").innerHTML =  day-4 + "/" + month + "/" + year;
-// document.querySelector(".day-5").innerHTML =  day-5 + "/" + month + "/" + year;
-// document.querySelector(".day-6").innerHTML =  day-6 + "/" + month + "/" + year;
-// document.querySelector(".day-7").innerHTML =  day-7 + "/" + month + "/" + year;
-
-// let currentDate = `${day}-${month}-${year}`;
-// console.log(currentDate); 
-
-
-// =================================================================
 // random telegram
 function randomTlg() {
     var telegram = [
@@ -58,12 +26,11 @@ function randomTlg() {
 }
 
 
-//  random zalo
+// random zalo
 function randomZalo() {
     var zalo = [
         'https://zalo.me/g/wzlgcw201',
-        'https://zalo.me/g/mtwifk462',
-        'https://zalo.me/g/ojucae708'
+        'https://zalo.me/g/mtwifk462'
     ]
     var i = parseInt(Math.random() * zalo.length);
     location.href = zalo[i];
